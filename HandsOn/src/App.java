804b9131214d8f72e1e0016a9f4809397565081d
Alexandre Direito <xandedsj@gmail.com>
2024-06-11 18:20:03 -0300

    Carrinho de compras feito!

--- HandsOn/src/App.java
+++ HandsOn/src/App.java
@@ -1,18 +1,22 @@
 import OperacoesBasicas.exc1.ListaDeTarefas;
+import OperacoesBasicas.exc2.CarrinhoDeCompra;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        ListaDeTarefas listaTarefa = new ListaDeTarefas();
+        CarrinhoDeCompra carrinhoDeCompra = new CarrinhoDeCompra();
 
-        listaTarefa.adicionarTarefa("Tarefa 1");
-        listaTarefa.adicionarTarefa("Tarefa 2");
-        listaTarefa.adicionarTarefa("Tarefa 3");
-        System.out.println("O numero total de elementos é: " + listaTarefa.obterNumeroTotalTarefas());
-        listaTarefa.obterDescricoesTarefas();
+        carrinhoDeCompra.adicionarItem("banana", 2d, 5);
+        carrinhoDeCompra.adicionarItem("maçã", 5d, 2);
+        carrinhoDeCompra.adicionarItem("uva", 1d, 3);
+        carrinhoDeCompra.adicionarItem("abóbora", 10d, 1);
+        carrinhoDeCompra.exibirItens();
 
-        listaTarefa.removerTarefa("Tarefa 2");
-        System.out.println("O numero total de elementos é: " + listaTarefa.obterNumeroTotalTarefas());
-        listaTarefa.obterDescricoesTarefas();
+        System.out.println("O valor total é: " + carrinhoDeCompra.calcularValorTotal() + " reais.");
+
+        carrinhoDeCompra.removerItem("banana");
+        carrinhoDeCompra.exibirItens();
+        
+        System.out.println("O valor total é: " + carrinhoDeCompra.calcularValorTotal() + " reais.");
     }
 }
--- HandsOn/src/OperacoesBasicas/exc2/CarrinhoDeCompra.java
+++ HandsOn/src/OperacoesBasicas/exc2/CarrinhoDeCompra.java
@@ -1,5 +1,66 @@
 package OperacoesBasicas.exc2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoDeCompra {
 
+    //atributo
+    private List<Item> compraCarrinho;
+
+    public CarrinhoDeCompra(List<Item> compraCarrinho) {
+        this.compraCarrinho = compraCarrinho;
+    }
+
+    //construtor
+    public CarrinhoDeCompra() {
+        this.compraCarrinho = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        Item item = new Item(nome, preco, quantidade);
+        this.compraCarrinho.add(item);
+    }
+
+    public void removerItem(String nome) {
+        List<Item> remocaoItems = new ArrayList<>();
+        if (!compraCarrinho.isEmpty()) {
+            for (Item item : compraCarrinho) {
+                if (item.getNome().equalsIgnoreCase(nome)) {
+                    remocaoItems.add(item);
+                }
+            }
+            compraCarrinho.removeAll(remocaoItems);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0d;
+        if (!compraCarrinho.isEmpty()) {
+          for (Item item : compraCarrinho) {
+            double valorItem = item.getPreco() * item.getQuantidade();
+            valorTotal += valorItem; //valorTotal = valorTotal + valorItem;
+          }
+          return valorTotal;
+        } else {
+          throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public void exibirItens() {
+        if(!compraCarrinho.isEmpty()) {
+            System.out.println(this.compraCarrinho);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    @Override
+    public String toString() {
+      return "CarrinhoDeCompras{" +
+          "itens=" + compraCarrinho +
+          '}';
+    }
 }
