8992c3fad8ae8d61f71128d27b639a59c2bf4900
Alexandre Direito <xandedsj@gmail.com>
2024-06-12 12:58:45 -0300

    Pesquisa em Map (Estoque de Produtos)

--- HandsOn/src/App.java
+++ HandsOn/src/App.java
@@ -1,18 +1,8 @@
-import Map.OperacoesBasicasEmMap.AgendaContatos;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        AgendaContatos agendaContatos = new AgendaContatos();
-
-        //trabalhando com o método put, servindo tanto pra atualizar como para adicionar.
-        //se repetir a chave, o put faz o replace.
-        agendaContatos.adicionarContato("Alexandre", 134);
-        agendaContatos.adicionarContato("Bianca", 576);
-        agendaContatos.adicionarContato("Alebi", 896);
-        agendaContatos.adicionarContato("Biale", 125);
-
-        agendaContatos.exibirContatos();
-
+        
     }   
 }
