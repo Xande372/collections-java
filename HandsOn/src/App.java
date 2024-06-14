ad0b59e1eb161e93a52d673463a8288589507b2a
Alexandre Direito <xandedsj@gmail.com>
2024-06-14 09:30:45 -0300

    Operações Básicas em Map (dicionário)

--- HandsOn/src/App.java
+++ HandsOn/src/App.java
@@ -1,8 +1,16 @@
-
+import Map.OperacoesBasicasEmMap.Dicionario;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        
+        Dicionario dicionario = new Dicionario();
+
+        dicionario.adicionarPalavra("rápido", "algo que não se move lento");
+        dicionario.adicionarPalavra("devagar", "algo que não se move rápido");
+
+        dicionario.exibirPalavras();
+        dicionario.pesquisarPorPalavra("devagar");
+        dicionario.removerPalavra("rápido");
+        dicionario.exibirPalavras();
     }   
 }
---
+++ HandsOn/src/Map/OperacoesBasicasEmMap/Dicionario.java
@@ -0,0 +1,37 @@
+package Map.OperacoesBasicasEmMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dicionario {
+
+    private Map<String, String> dicionarioMap;
+
+    public Dicionario() {
+        this.dicionarioMap = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, String definicao) {
+        dicionarioMap.put(palavra, definicao);
+    }
+
+    public void removerPalavra(String palavra) {
+        if (!dicionarioMap.isEmpty()) {
+            dicionarioMap.remove(palavra);
+        } else {
+            System.out.println("O dicionário está vazio!");
+        }
+    }
+
+    public void exibirPalavras() {
+        if (!dicionarioMap.isEmpty()) {
+            System.out.println(dicionarioMap);
+        } else {
+            System.out.println("O dicionário está vazio!");
+        }
+    }
+
+    public void pesquisarPorPalavra(String palavra) {
+        dicionarioMap.get(palavra);
+    }
+}
